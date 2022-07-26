7f3ba01b3bd41782649a923b425c300917782772
InterCab <intercab@intraudesa.edu.ar>
2022-07-26 09:38:41 -0300

    hasta punto 3 HECHO / mostrar en consola - sin espacios dobles - apellidos mayuscula

--- bc_592066_ejercicio_12-Integrante1-Agustina-Paso_Integrante2-Emanuel_Some.zip/js/main.js
+++ bc_592066_ejercicio_12-Integrante1-Agustina-Paso_Integrante2-Emanuel_Some.zip/js/main.js
@@ -5,68 +5,35 @@ console.log(document.querySelector('title').innerText)
 
 /* ----- 3) ----- */
 
-/* const datosIntegrantes = document.querySelectorAll('dd')
-console.log(datosIntegrantes
-    )
+const vectorNombresInt_1 = document.querySelectorAll('.int1_N')
+const vectorApellidosInt_1 = document.querySelectorAll('.int1_A')
+const vectorNombresInt_2 = document.querySelectorAll('.int2_N')
+const vectorApellidosInt_2 = document.querySelectorAll('.int2_A')
 
-function mostrarDatosEnConsola(){
-datosIntegrantes.forEach(dato => {
-    return primerIntegrante += dato.textContent
-});
+const nombresInt1 = concatenarStrings(vectorNombresInt_1)
+const apellidosInt1 = concatenarStrings(vectorApellidosInt_1)
+const nombresInt2 = concatenarStrings(vectorNombresInt_2)
+const apellidosInt2 = concatenarStrings(vectorApellidosInt_2)
 
-console.log(`---------
-Integrante 1: ${primerIntegrante}
-Integrante 2: "aaa"
----------
+function concatenarStrings(vector){
+    let stringAMostrar = ""
+    let longitudVector = vector.length
+
+    for (let i = 0; i < longitudVector; i++) {
+        if (vector[i].textContent !== "") {
+            stringAMostrar += vector[i].textContent + " "
+        }
+    }
+
+    return stringAMostrar
+}
+
+console.log(
+    `
+----------
+Integrante 1: ${nombresInt1.trim()} ${apellidosInt1.toUpperCase().trim()}
+Integrante 2: ${nombresInt2.trim()} ${apellidosInt2.toUpperCase().trim()}
+----------
 `)
-}
 
-mostrarDatosEnConsola() */
 
-const nombresInt_1 = document.querySelectorAll('.int1_N')
-const apellidosInt_1 = document.querySelectorAll('.int1_A')
-let nom = ""
-let ape = ""
-let longitudN = nombresInt_1.length
-let longitudA = apellidosInt_1.length
-
-console.log(longitudN)
-console.log(longitudA)
-
-console.log(nombresInt_1)
-console.log(apellidosInt_1)
-
-/* apellidosInt_1.forEach((apellido,i) => {
-    ape += apellido[i].textContent 
-}); */
-
-for (let i = 0; i < longitudN; i++) {
-    if (nombresInt_1[i].textContent !== "") {
-        nom += nombresInt_1[i].textContent + " "
-    }
-}
-
-for (let i = 0; i < longitudA; i++) {
-    if (apellidosInt_1[i].textContent !== "") {
-        ape += apellidosInt_1[i].textContent + " "
-    }
-}
-
-console.log({nom})
-console.log({ape})
-
-console.log(`Integrante 1: ${nom.trim()} ${ape.trim()}`)
-
-/* let apellidosEnMayuscula = apellidosInt_1.map(function(apel){
-    return apel.innerText;
-}) */
-
-/* console.log(apellidosEnMayuscula) */
-
-/* 
-function mostrarDatosConsola(){
-    array.forEach(element => {
-        
-    });
-}
-console.log(nombresInt_1[0].textContent + " " + nombresInt_1[1].textContent + " " + apellidosInt_1[0].textContent) */
